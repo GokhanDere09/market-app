55054298220cd343f36b91582982fa3bbc3df6f4
Gökhan Dere <gokhandere4209@gmail.com>
2026-09-25 09:36:36 +0300

    Fix formatting issues in db.py

--- backend/db.py
+++ backend/db.py
@@ -1,13 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./market.db" #SQLite veritabanı 
+DATABASE_URL = "sqlite:///./market.db" 
 
 engine = create_engine( #veritabanı bağlantı motoru
     DATABASE_URL,
-    connect_args={"check_same_thread": False} #SQLite'ın aynı anda birden fazla bağlantıya izin vermemesini engeller
+    connect_args={"check_same_thread": False} 
 )
 
-SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False) #veritabanı oturumları oluşturmak için kullanılan bir sınıf
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False) 
 
 Base = declarative_base()
